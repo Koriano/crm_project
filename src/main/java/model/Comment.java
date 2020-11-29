adec5f1488f82dfdb5ae80b87ea94a0b9d26a32e
margauxsch <64523258+margauxsch@users.noreply.github.com>
2020-11-29 16:40:53 +0100

    assert deleted

--- src/main/java/model/Comment.java
+++ src/main/java/model/Comment.java
@@ -4,8 +4,6 @@ package model;
  * This class represents a comment in the CRM
  *
  * @author Margaux SCHNELZAUER
- *
- * @inv this.author != null && this.concernedContact != null && this.content != null
  */
 public class Comment {
 
@@ -38,18 +36,13 @@ public class Comment {
      * @param concernedContact : the contact who is concerned by the comment
      * @param id : the comment id
      *
-     * @pre author != null & concernedContact != null && content != null
      */
     public Comment(Contact author, Contact concernedContact, String content, int id) {
-        // pre condition
-        assert author != null & concernedContact != null && content != null: "Pre condition violated";
-
         this.author = author;
         this.concernedContact = concernedContact;
         this.content = content;
         this.id = id;
 
-        this.inv();
     }
 
     /**
@@ -57,19 +50,14 @@ public class Comment {
      *
      * @param author : the comment author
      * @param concernedContact : the contact who is concerned by the comment
-     *
-     * @pre author != null & concernedContact != null && content != null
      */
     public Comment(Contact author, Contact concernedContact, String content) {
-        // pre condition
-        assert author != null & concernedContact != null && content != null: "Pre condition violated";
 
         this.author = author;
         this.concernedContact = concernedContact;
         this.content = content;
         this.id = 0;
 
-        this.inv();
     }
 
     //-----------------------------------------------------------------//
@@ -80,7 +68,7 @@ public class Comment {
      * @return the comment content
      */
     public String getContent() {
-        this.inv();
+
         return this.content;
     }
 
@@ -90,7 +78,7 @@ public class Comment {
      * @return the contact author
      */
     public Contact getAuthor() {
-        this.inv();
+
         return this.author;
     }
 
@@ -100,7 +88,7 @@ public class Comment {
      * @return the contact who is concerned by the comment
      */
     public Contact getConcernedContact() {
-        this.inv();
+
         return this.concernedContact;
     }
 
@@ -119,62 +107,37 @@ public class Comment {
      * Set a new content in the comment
      *
      * @param content : the new content
-     *
-     * @pre content != null
      */
     public void setContent(String content) {
-        // pre condition
-        assert content != null : "Pre condition violated";
-
         this.content = content;
-        this.inv();
     }
 
     /**
      * Set a new author
      *
      * @param author : the new comment author
-     *
-     * @pre author != null
      */
     public void setAuthor(Contact author) {
-        // pre condition
-        assert author != null : "Pre condition violated";
-
         this.author = author;
-        this.inv();
     }
 
     /**
      * Set a new concerned contact
      *
-     * @param concernedContact : the new concerned contact
-     *
-     * @pre concernedContact != null
+     * @param concernedContact : new concerned contact
      */
     public void setConcernedContact(Contact concernedContact) {
-        // pre condition
-        assert concernedContact != null : "Pre condition violated";
-
         this.concernedContact = concernedContact;
-        this.inv();
     }
 
     /**
      * Set a new comment id
      *
-     * @param id : the new comment id
+     * @param id : id of comment
      */
     public void setId(int id) {
         this.id = id;
     }
-    //-----------------------------------------------------------------//
 
-    /**
-     * The invariant of the class
-     */
-    private void inv(){
-        assert this.author != null && this.concernedContact != null && this.content != null: "Invariant violated";
-    }
 
 }
--- src/main/java/model/Event.java
+++ src/main/java/model/Event.java
@@ -7,8 +7,6 @@ import java.util.Date;
  * This class represents an Event in the CRM
  *
  * @author Margaux SCHNELZAUER
- *
- * @inv name != null && this.date != null && this.contactsList.size() > 0
  */
 public class Event {
 
@@ -51,13 +49,8 @@ public class Event {
      * @param author : the event author
      * @param type : the event type
      * @param id : the event id
-     *
-     * @pre name != null && date != null  && author != null && !type.isEmpty() && type != null
      */
     public Event(String name, Date date, Contact author, String type, int id) {
-        // pre conditon
-        assert name != null && date != null && author != null  && !type.isEmpty() && type != null: " Pre condition violated";
-
         this.name = name;
         this.date = date;
         this.contactsList = new ArrayList<Contact>();
@@ -65,8 +58,6 @@ public class Event {
         this.type = type;
         this.description = "";
         this.id = id;
-
-        this.inv();
     }
 
     /**
@@ -76,13 +67,8 @@ public class Event {
      * @param date : the event date
      * @param author : the event author
      * @param type : the event type
-     *
-     * @pre name != null && date != null  && author != null && !type.isEmpty() && type != null
      */
     public Event(String name, Date date, Contact author, String type) {
-        // pre conditon
-        assert name != null && date != null && author != null  && !type.isEmpty() && type != null: " Pre condition violated";
-
         this.name = name;
         this.date = date;
         this.contactsList = new ArrayList<Contact>();
@@ -90,8 +76,6 @@ public class Event {
         this.type = type;
         this.description = "";
         this.id = 0;
-
-        this.inv();
     }
 
     //-----------------------------------------------------------------//
@@ -112,7 +96,6 @@ public class Event {
      * @return the event name
      */
     public String getName() {
-        this.inv();
         return this.name;
     }
 
@@ -122,7 +105,6 @@ public class Event {
      * @return the event description
      */
     public String getDescription() {
-        this.inv();
         return this.description;
     }
 
@@ -133,7 +115,6 @@ public class Event {
      * @return the event date
      */
     public Date getDate() {
-        this.inv();
         return this.date;
     }
 
@@ -143,7 +124,6 @@ public class Event {
      * @return the event type
      */
     public String getType() {
-        this.inv();
         return this.type;
     }
 
@@ -153,7 +133,6 @@ public class Event {
      * @return a list of contact concerned by the event
      */
     public ArrayList<Contact> getContactsList() {
-        this.inv();
         return this.contactsList;
     }
 
@@ -173,105 +152,54 @@ public class Event {
      * Set a new name
      *
      * @param name : the new event name
-     *
-     * @pre name != null
      */
     public void setName(String name) {
-        // pre condition
-        assert name != null : "Pre condition violated";
-
         this.name = name;
-        this.inv();
     }
 
     /**
      * Set a new event description
      *
      * @param description : the new event description
-     *
-     * @pre description != null
      */
     public void setDescription(String description) {
-        // pre condition
-        assert description != null : "Pre condition violated";
-
         this.description = description;
-        this.inv();
     }
 
     /**
      * Set a new event date
      *
      * @param date : the new event date
-     *
-     * @pre date != null
      */
     public void setDate(Date date) {
-        // pre condition
-        assert date != null : "Pre condition violated";
-
         this.date = date;
-        this.inv();
     }
 
     /**
      * Set a new event type
      *
      * @param type : the new event type
-     *
-     * @pre !type.isEmpty() && type != null
      */
     public void setType(String type) {
-        // pre condition
-        assert !type.isEmpty() && type != null : "Pre condition violated";
-
         this.type = type;
-        this.inv();
     }
 
     /**
      * Add a new contact in the contacts list
      *
      * @param contact : the new contact to add
-     *
-     * @pre contact != null && !this.contactsList.contains(contact)
-     * @post this.contactsList.contains(contact)
      */
     public void addContact(Contact contact){
-        // pre condition
-        assert contact != null && !this.contactsList.contains(contact) : "Pre condition violated";
-
         this.contactsList.add(contact);
-
-        // post condition
-        assert this.contactsList.contains(contact) : "Post condition violated";
-        this.inv();
     }
 
     /**
      * Remove a contact from the contacts list
      *
      * @param contact : the contact to remove
-     *
-     * @pre contact != null && this.contactsList.contains(contact)
-     * @post !this.contactsList.contains(contact)
      */
     public void removeContact(Contact contact){
-        // pre condition
-        assert contact != null && this.contactsList.contains(contact) : "Pre condition violated";
-
         this.contactsList.remove(contact);
-
-        // post condition
-        assert !this.contactsList.contains(contact) : "Post condition violated";
-        this.inv();
     }
-    //-----------------------------------------------------------------//
 
-    /**
-     * The invariant of the class
-     */
-    private void inv(){
-        assert name != null && this.date != null && this.contactsList.size() > 0 : "Invariant violated";
-    }
 }
